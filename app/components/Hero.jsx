80b427300b04fd95ed978761c304487b97c5a01a
Adil Aslam <aaliyanaslam22@gmail.com>
2026-02-25 00:07:58 +0500

    Update Hero.jsx

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -32,15 +32,15 @@ const Hero = () => {
   return (
     <section className="w-full bg-white overflow-hidden">
       <div className="max-w-360 mx-auto px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 2xl:px-20 3xl:px-28
-                      py-12 sm:py-20 md:py-24 lg:py-28 xl:py-28">
+                      py-6 sm:py-8 md:py-24 lg:py-12 xl:py-18">
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-center">
 
           {/* LEFT CONTENT */}
-          <div className="space-y-8 lg:space-y-10">
+          <div className="space-y-2 lg:space-y-6">
 
             {/* Heading */}
-            <div className="space-y-6">
+            <div className="space-y-2 lg:space-y-6">
               <h1
                 className={`
                 ${urbanist.className}
@@ -51,7 +51,7 @@ const Hero = () => {
               `}
               >
                 Auto Repair & Maintenance
-                <span className="block text-blue-600">
+                <span className="block  text-blue-600">
                   Report Provider
                 </span>
               </h1>
